cbb86c8640332b84b4e0cf4bc1d0941cbae3f113
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-05-29 15:07:06 +0100

    Changing header code

--- perch/templates/layouts/main-header.php
+++ perch/templates/layouts/main-header.php
@@ -7,7 +7,6 @@
 	<?php $page_css_file	=  perch_page_attribute('css', array('template' => 'bits.html'), true); ?>
 	<link rel="stylesheet" href="<?php echo $page_css_file ?>?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].$page_css_file);?>">
 	<!-- Typekit  -->
-	<link rel="stylesheet" href="https://use.typekit.net/xvs0zel.css">
 	<!-- Perch Meta -->
 	<?php
 	$domain        = 'https://'.$_SERVER["HTTP_HOST"];
@@ -17,9 +16,9 @@
 										    'add-trailing-slash' => false,
 										    'include-domain'     => true,
 										), true);
-	$mainsitename      = "phoenix & phoenix";
-	$pagetitlename = " - phoenix & phoenix";
-	$sharing_image = '/images/default_fb_image.jpg';
+	$mainsitename      = "XXXX";
+	$pagetitlename = " - XXXX";
+	$sharing_image = '/images/default-sharing-image.jpg';
 
 	PerchSystem::set_var('domain',$domain);
 	PerchSystem::set_var('mainurl',$mainurl);
@@ -41,12 +40,12 @@
 				<!-- Skip to main content link  -->
 				<a class="show-on-focus"  href="#main-content">Skip to main content</a>
 				<!-- For SVG logo. Delete if not needed -->
-				<!-- <div class="main-logo-wrapper">
+				<div class="main-logo-wrapper">
 					<a class="main-logo-wrapper" itemprop="url" href="<?php echo $domain ?>">
 					<img class="hidden-main-logo" aria-hidden="true" itemprop="logo" src="/images/main-logo.png" alt="phoenix and phoenix main logo">
 					<?php perch_content("Main Logo"); ?>
 				</a>
-				</div> -->
+				</div>
 					<?php perch_pages_navigation(array(
 							'hide-extensions' => true,
 							'levels'    => 1
