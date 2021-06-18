743b7527b0af5b95dfa53d20eaf9c01b032662ef
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-06-18 17:57:07 +0100

    Contact form

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <main class="main-content homepage-main-content" id="main-content">
 	  <?php perch_content('Section One'); ?>
 	  <?php perch_content('Homepage Gallery'); ?>
+	  <?php perch_content('Contact Form'); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('main-footer-end'); ?>
