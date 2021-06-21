532674fe3d64e8952ebdaf322220a11fe3e425ae
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-06-21 18:13:29 +0100

    About us page

--- about-me.php
+++ about-me.php
@@ -2,9 +2,11 @@
 <?php perch_layout('about-me-header'); ?>
 <main class="main-content about-me-main-content" id="main-content">
   <?php perch_content("Page Content"); ?>
+  <?php perch_content('Contact Form'); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('about-me-footer-end'); ?>
+
 <!-- TODO: Remove debug -->
 <?php PerchUtil::output_debug(); ?>
 <!-- TODO: Validate HTML -->
