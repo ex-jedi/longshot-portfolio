3e987b72e3ceb80258e9cd62d2d485428ac7ecae
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-06-22 15:12:16 +0100

    Error page

--- 404.php
+++ 404.php
@@ -1,6 +1,5 @@
 <?php include('perch/runtime.php'); ?>
-<?php perch_layout('main-header'); ?>
-<?php perch_layout("empty-header-end"); ?>
+<?php perch_layout('error-page-header'); ?>
 <main class="main-content error-page-main-content" id="main-content">
 	<?php perch_content("404 Page Content"); ?>
 </main>
