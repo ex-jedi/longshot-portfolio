0bc56181a9576c90ceb1f66e2e0fda297167d5ab
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-07-02 12:34:22 +0100

    Ready for animatins

--- scripts/js/animations.js
+++ scripts/js/animations.js
@@ -80,100 +80,14 @@ function menuOpenerHandler() {
 // ** GSAP Animations For Multiple Pages  **
 // *==============================================================================
 
-// *=========================================
-// ** Fading in paragraphs  **
-// *=========================================
-
-function fadeInRotateParagraphs() {
-  // * Adding class to paragraphs created by Perch to set them up for fading in.
-  // Grabbing paragraphs from multiple pages
-  const aboutMeParagraphs = Array.from(document.querySelectorAll('.about-me-section p'));
-  const servicesParagraphs = Array.from(document.querySelectorAll('.services-section p'));
-  // Merge the paragraphs into one array
-  const fadeInParagraph = [...aboutMeParagraphs, ...servicesParagraphs];
-  // Add class to paragraphs
-  fadeInParagraph.forEach((paragraph) => paragraph.classList.add('fade-in-rotate'));
-
-  // Grabbing all paragraphs to fade in
-  const fadeInParagraphs = gsap.utils.toArray('.fade-in-rotate');
-
-  fadeInParagraphs.forEach((paragraph) => {
-    ScrollTrigger.matchMedia({
-      // desktop
-      '(min-width: 1100px)': function () {
-        ScrollTrigger.create({
-          trigger: paragraph,
-          toggleClass: 'fade-in-rotate-reveal',
-          start: 'top 95%',
-          end: 'bottom top',
-        });
-      },
-
-      // Tablet
-      '(max-width: 1099px) and (min-width: 700px)': function () {
-        ScrollTrigger.create({
-          trigger: paragraph,
-          toggleClass: 'fade-in-rotate-reveal',
-          start: 'top bottom',
-          end: 'bottom -100px',
-        });
-      },
-
-      // Mobile
-      '(max-width: 699px) and (min-width: 450px)': function () {
-        ScrollTrigger.create({
-          trigger: paragraph,
-          toggleClass: 'fade-in-rotate-reveal',
-          start: 'top bottom',
-          end: 'bottom -300px',
-        });
-      },
-
-      // Small Mobile
-      '(max-width: 449px)': function () {
-        ScrollTrigger.create({
-          trigger: paragraph,
-          toggleClass: 'fade-in-rotate-reveal',
-          start: 'top bottom',
-          end: 'bottom -450px',
-        });
-      },
-    });
-  });
-}
-
-// *=========================================
-// ** Contact form color change  **
-// *=========================================
-
-function scrollColourChange() {
-  ScrollTrigger.create({
-    trigger: '.general-contact-form-section',
-    start: 'top center',
-    end: 'bottom center',
-    id: 'Contact Form',
-    toggleClass: {
-      targets: '.general-contact-form-section, .general-contact-form-input, .main-contact-submit-button',
-      className: 'contact-form-colour-change',
-    },
-  });
-}
-
 // *==============================================================================
+// ** Page Animations  **
+// *==============================================================================
+
+// *=========================================
 // ** Homepage  **
-// *==============================================================================
+// *=========================================
 
-
-ScrollTrigger.batch('.fade-in-rotate', {
-  start: 'top 98%',
-  markers: true,
-  // interval: 0.1, // time window (in seconds) for batching to occur.
-  // batchMax: 3,   // maximum batch size (targets)
-  onEnter: (batch) => gsap.to(batch, { opacity: 1, rotateX: 0, ease: 'power1.in' }),
-  onLeaveBack: (batch) => gsap.to(batch, { opacity: 0, rotateX: 90, ease: 'power1.in' }),
-  onEnterBack: (batch) => gsap.to(batch, { opacity: 1, rotateX: 0, ease: 'power1.in' }),
-  // you can also define things like start, end, etc.
-});
 
 // ********** Homepage Parallax Image **********
 
