8a9a952b6714392f456985e43d29caec29246f86
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-07-09 18:53:52 +0100

    header content image change

--- terms.php
+++ terms.php
@@ -5,6 +5,3 @@
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('empty-footer-end'); ?>
-<!-- TODO: Remove debug -->
-<!-- <?php PerchUtil::output_debug(); ?> -->
-<!-- TODO: Validate HTML -->
